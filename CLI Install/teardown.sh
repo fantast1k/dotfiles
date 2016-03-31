66e8067e38dbfaa2b4591a1bfdab63d7b3d95242
Fa[n]tastik <dmitry.fantastik@gmail.com>
2016-03-31 12:56:56 +0300

    Add teardown script for CLI Installs

---
+++ CLI Install/teardown.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# At first prepare our script for run correctly
+
+# Enable Assitive Touch for Terminal so we don't need 
+# to answer popped up windows and can use scripts
+sudo rv /private/var/db/.AccessibilityAPIEnabled
+
+# Add sqlite entry that allows terminal for assistive touch
+sudo sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db "UPDATE access SET allowed=0 WHERE service='kTCCServiceAccessibility' AND client='com.apple.Terminal'"
