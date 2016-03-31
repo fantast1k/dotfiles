d807d7a83c2335eb5d711c8cd34d9fcecb4878c6
Fa[n]tastik <dmitry.fantastik@gmail.com>
2016-03-31 12:58:08 +0300

    Add google chrome install script to CLI Installs

---
+++ CLI Install/google_chrome.sh
@@ -0,0 +1,7 @@
+temp=$TMPDIR$(uuidgen)
+mkdir -p $temp/mount
+curl https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg > $temp/1.dmg
+yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/1.dmg
+cp -r $temp/mount/*.app /Applications
+hdiutil detach $temp/mount
+rm -r $temp
